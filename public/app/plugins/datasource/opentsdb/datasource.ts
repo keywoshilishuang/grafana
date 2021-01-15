b5c3b03a7f83e59b0d771470ff7cd7bd7fc19c6a
stevensli <931400503@qq.com>
2021-01-15 12:01:24 +0800

    bugfix

--- public/app/plugins/datasource/opentsdb/datasource.ts
+++ public/app/plugins/datasource/opentsdb/datasource.ts
@@ -214,12 +214,12 @@ export default class OpenTsDatasource extends DataSourceApi<OpenTsdbQuery, OpenT
     );
   }
 
-  _randomRangeId(num: number){
-    var returnStr = "";
+  _randomRangeId(num: number) {
+    var returnStr = '';
     var charStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for(var i=0; i<num; i++){
-      var index = Math.round(Math.random() * (charStr.length-1));
-      returnStr += charStr.substring(index,index+1);
+    for(var i = 0; i < num; i++){
+      var index = Math.round(Math.random() * (charStr.length - 1));
+      returnStr += charStr.substring(index, index+1);
     }
     return returnStr;
   }
