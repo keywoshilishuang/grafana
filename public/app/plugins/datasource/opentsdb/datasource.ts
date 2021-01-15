c6e68bb07b6caf0760b03b3ec36b55344bd3a195
stevensli <931400503@qq.com>
2021-01-15 12:04:24 +0800

    bugfix

--- public/app/plugins/datasource/opentsdb/datasource.ts
+++ public/app/plugins/datasource/opentsdb/datasource.ts
@@ -217,9 +217,9 @@ export default class OpenTsDatasource extends DataSourceApi<OpenTsdbQuery, OpenT
   _randomRangeId(num: number) {
     var returnStr = '';
     var charStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for(var i = 0; i < num; i++){
+    for (var i = 0; i < num; i++) {
       var index = Math.round(Math.random() * (charStr.length - 1));
-      returnStr += charStr.substring(index, index+1);
+      returnStr += charStr.substring(index, index + 1);
     }
     return returnStr;
   }
